1eb8ab3ca5b7503225b7e6c63305097c6a2d72f2
Aparna <aparnasonker615@gmil.com>
2022-10-21 19:39:25 +0530

    Initial commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+    prompt: "What is 1+3?",
+    optionA: "24",
+    optionB: "21",
+    optionC: "4",
+    optionD: "2",
+    answer: "C",
+    },
+    {
+        prompt:"Which is  a Streaming Platform?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "Rodinhood",
+        answer: "B",
+    },
+    {
+        prompt: "Who is the first Prime Minster of India?",
+        optionA:"Pt. Jawaharlal Nehru",
+        optionB: "Indira Gandhi",
+        optionC: "Lal Bahadur Shastri",
+        optionD: "Subhsh Chandra Bosh",
+        answer: "A",
+    },
+    {
+        prompt: "Best tech youtube channel?",
+        optionA: "PedroTech",
+        optionB: "Dev Ed",
+        optionC: "Fireship",
+        optionD: "TechWithTim",
+        answer: "A",
+    },
+];
